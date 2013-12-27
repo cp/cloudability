6f3db4d6bc1f4f9d76e0ed636e8cec9fae50f358
Colby Aley <colby@aley.me>
2013-12-26 21:25:44 -0800

    Whoops! I was testing cost in the usage spec.

--- spec/cloduability/usage_spec.rb
+++ spec/cloduability/usage_spec.rb
@@ -6,26 +6,26 @@ describe Cloudability::Client::UsageReports do
     @client = Cloudability::Client.new(auth_token: 'token')
   end
 
-  describe '#cost_reports' do
+  describe '#usage_reports' do
     it 'should be an array' do
-      stub_get('/1/reporting/cost?auth_token=token', 'cost/reports')
-      @client.cost_reports.should be_kind_of Array
+      stub_get('/1/reporting/usage?auth_token=token', 'usage/reports')
+      @client.usage_reports.should be_kind_of Array
     end
 
     it 'should be an array of Hashie::Mashes' do
-      stub_get('/1/reporting/cost?auth_token=token', 'cost/reports')
-      @client.cost_reports.each{|report| report.should be_kind_of Hashie::Mash }
+      stub_get('/1/reporting/usage?auth_token=token', 'usage/reports')
+      @client.usage_reports.each{|report| report.should be_kind_of Hashie::Mash }
     end
   end
 
   describe '#usage_measures' do
     it 'should be an array' do
-      stub_get('/1/reporting/measures?auth_token=token', 'cost/measures')
+      stub_get('/1/reporting/measures?auth_token=token', 'usage/measures')
       @client.usage_measures.should be_kind_of Array
     end
 
     it 'should be an array of Hashie::Mashes' do
-      stub_get('/1/reporting/measures?auth_token=token', 'cost/measures')
+      stub_get('/1/reporting/measures?auth_token=token', 'usage/measures')
       @client.usage_measures.each{|measure| measure.should be_kind_of Hashie::Mash }
     end
   end
